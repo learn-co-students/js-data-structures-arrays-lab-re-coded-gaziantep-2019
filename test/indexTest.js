fed0b093904418b0c883abb3f6ccdfb032002cbf
Eren Dogan <eren.dogan@gaziulas.com.tr>
2019-06-09 14:18:03 +0000

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -66,13 +66,13 @@ describe('index.js', function () {
       });
     });
 
-    describe('removeLastDriver()', function () {
+    /* describe('removeLastDriver()', function () {
       it('removes the last driver in the drivers array and returns a new array, leaving the drivers array unchanged', function () {
         expect(removeLastDriver()).to.have.ordered.members(["Milo", "Otis"]);
 
         expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
       });
-    });
+    }); */
 
     describe('removeFirstDriver()', function () {
       it('removes the first driver from the drivers array and returns a new array, leaving the drivers array unchanged', function () {
